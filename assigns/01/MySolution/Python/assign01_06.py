2fd3e7868f85a28a7118bfab3a08e5c312b9f560
unknown <easonlhc@bu.edu>
2023-06-04 20:26:59 -0400

    add 01

--- assigns/01/MySolution/Python/assign01_06.py
+++ assigns/01/MySolution/Python/assign01_06.py
@@ -3,7 +3,6 @@ import sys
 sys.path.append('./../../')
 from assign01_lib import *
 from assign01_05 import *
-
 ####################################################
 print("[import ./../../assign01_lib.py] is done!")
 ####################################################
@@ -50,7 +49,8 @@ def mylist_quicksort(xs):
             ys,zs = qpart(xs,x1)
             ys = qsort(ys)
             zs = qsort(zs)
-            return mylist_append(ys,mylist_cons(x1,zs))
+            x1 = mylist_cons(x1, mylist_nil())
+            return append_lists(ys,mylist_cons(x1,zs))
         
     def qpart(xs,p0):
         if mylist_nilq(xs):
@@ -59,10 +59,21 @@ def mylist_quicksort(xs):
             x1 = xs.get_cons1()
             xs = xs.get_cons2()
             ys,zs = qpart(xs,p0)
-            if x1<=p0:
-                return mylist_cons(x1, ys), mylist_cons(zs  , mylist_nil())
+            if x1 <= p0:
+                return mylist_cons(x1, ys), zs
             else:
-                return mylist_cons(ys, mylist_nil()), mylist_cons(x1, zs)
-             
+                return ys, mylist_cons(x1, zs)
+            
     return qsort(mylist_cons(xs, mylist_nil()))
 
+def append_lists(list1, list2):
+        if mylist_nilq(list1):
+            return list2
+        elif mylist_nilq(list2):
+            return list1
+        else:
+            current = list1
+        while mylist_consq(current.get_cons2()):
+            current = current.get_cons2()
+        current.cons2 = list2
+        return list1
