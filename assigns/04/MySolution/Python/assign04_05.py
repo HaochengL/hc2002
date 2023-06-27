f278b8f72a8d6a7b5d8ee266e4b6f9fa5380a4c4
unknown <easonlhc@bu.edu>
2023-06-27 15:48:51 -0400

    add 04

--- assigns/04/MySolution/Python/assign04_05.py
+++ assigns/04/MySolution/Python/assign04_05.py
@@ -18,7 +18,44 @@ word as the player's guess. THIS GUESS SHOULD NOT CONTRADICT
 ANY ONE OF THE HINTS GIVEN. def wordle_guess(hints):
 """
 ########################################################################
+def wordle_guess(hints):
+    
+    possibilities = [set() for _ in range(len(hints[0]))]
 
 
+    placed_chars = set()
 
-########################################################################
+
+    for hint in hints:
+        for index, (color, char) in enumerate(hint):
+            if color == 1:
+                possibilities[index] = {char}
+                placed_chars.add(char)
+
+
+    for hint in hints:
+        for index, (color, char) in enumerate(hint):
+
+            if color == 0 or char in placed_chars:
+                continue
+
+            possible_positions = set()
+            for i, pos in enumerate(possibilities):
+                if i != index and not pos:
+                    possible_positions.add(i)
+
+            if len(possible_positions) == 1:
+                pos_index = possible_positions.pop()
+                possibilities[pos_index] = {char}
+                placed_chars.add(char)
+
+
+    remaining_chars = [char for hint in hints for (_, char) in hint if char not in placed_chars and _ == 2]
+    for i, pos in enumerate(possibilities):
+        if not pos:
+            possibilities[i] = {remaining_chars.pop(0)}
+
+    word = "".join([list(pos)[0] for pos in possibilities])
+
+    return word
+
